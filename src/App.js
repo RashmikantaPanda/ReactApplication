7cbbcf79f621960711bf4b8e08fbe7576ebf0591
RashmikantaPanda <rashmikantapanda1234@gmail.com>
2023-03-21 15:13:17 +0530

    Version 1.0

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './MyComponents/Header'
+import { Todos } from "./MyComponents/Todos";
+import { Footer } from './MyComponents/Footer'
+import { AddTodo } from './MyComponents/AddTodo';
+import React, { useState } from 'react';
 
 function App() {
+  const onDelete = (todo) => {
+    console.log("I am onDelete");
+    setTodos(todos.filter((e) => {
+      return e!==todo;
+    }));
+  }
+
+  const addTodo = (title, desc) => {
+    console.log("I am adding this todo", title, desc);
+    let sno = todos[todos.length - 1].sno+1;
+    const myTodo = {
+      sno: sno,
+      title: title,
+      desc: desc,
+    }
+    setTodos([...todos, myTodo]);
+    console.log(myTodo);
+  }
+
+  const [todos, setTodos] = useState([
+    {
+      sno: 1,
+      title: "Go to the market",
+      desc: "You have to go to the market to get this job done"
+    },
+    {
+      sno: 2,
+      title: "Go to the college",
+      desc: "You have to go to the market to get this job2 done"
+    },
+    {
+      sno: 3,
+      title: "Go to the Office",
+      desc: "You have to go to the market to get this job3 done"
+    },
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header title="My Todos List" searchBar={false} />
+      <AddTodo addTodo={addTodo} />
+      <Todos todos={todos} onDelete={onDelete} />
+      <Footer />
+    </>
   );
 }
 
